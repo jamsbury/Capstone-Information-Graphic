3831bb4da4d3fcbd795df8dfec769dee1951c772
Justice <justiceamsbury@gmail.com>
2019-05-15 11:59:10 -0700

    Fix

--- js/main.js
+++ js/main.js
@@ -26,7 +26,7 @@
 // IMPORTANT NOTE: When you make changes to the page, you may need to refresh for it to work correctly.
 
 $(function() {
-  $('a[href*=#]').on('click', function(e) {
+  $('.drop').on('click', function(e) {
     e.preventDefault();
     $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 250, 'linear');
   });
